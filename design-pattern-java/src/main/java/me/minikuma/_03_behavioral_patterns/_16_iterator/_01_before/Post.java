9631df4e6d5ace8bbfc6da2ff9232696f2aa4c15
minikuma <minikuma@naver.com>
2022-05-10 00:05:47 +0900

    [FEAT] iterator pattern

---
+++ design-pattern-java/src/main/java/me/minikuma/_03_behavioral_patterns/_16_iterator/_01_before/Post.java
@@ -0,0 +1,29 @@
+package me.minikuma._03_behavioral_patterns._16_iterator._01_before;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private String title;
+    private LocalDateTime createdDateTime;
+
+    public Post(String title) {
+        this.title = title;
+        this.createdDateTime = LocalDateTime.now();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+}
