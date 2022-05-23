ddb0256deec67db077240f2b37deab8ccf05f0de
minikuma <minikuma@naver.com>
2022-05-23 23:20:14 +0900

    [FEAT] status pattern

---
+++ design-pattern-java/src/main/java/me/minikuma/_03_behavioral_patterns/_20_status/_02_after/Status.java
@@ -0,0 +1,6 @@
+package me.minikuma._03_behavioral_patterns._20_status._02_after;
+
+public interface Status {
+    void addReview(String review, Student student);
+    void addStudent(Student student);
+}
