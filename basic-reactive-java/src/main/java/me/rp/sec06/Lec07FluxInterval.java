85910009a24e352a1ae8330af707f93caf67e434
minikuma <minikuma@naver.com>
2023-02-06 22:48:18 +0900

    [sec07] finished

---
+++ basic-reactive-java/src/main/java/me/rp/sec06/Lec07FluxInterval.java
@@ -0,0 +1,17 @@
+package me.rp.sec06;
+
+import me.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+public class Lec07FluxInterval {
+    public static void main(String[] args) {
+        Flux.interval(Duration.ofSeconds(1))
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(60);
+    }
+}
