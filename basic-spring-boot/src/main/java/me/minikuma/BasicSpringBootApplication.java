675ade98f11e214695d068391808cee2998004bb
minikuma <minikuma@naver.com>
2023-02-07 22:52:45 +0900

    Servlet 구현 (제공 클래스 활용)

--- basic-spring-boot/src/main/java/me/minikuma/BasicSpringBootApplication.java
+++ basic-spring-boot/src/main/java/me/minikuma/BasicSpringBootApplication.java
@@ -1,13 +1,34 @@
 package me.minikuma;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class BasicSpringBootApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(BasicSpringBootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.addServlet("hello", new HttpServlet() {
+                    @Override
+                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                        resp.setHeader("Content-Type", "text/plain");
+                        resp.setCharacterEncoding("UTF-8");
+                        resp.setStatus(200);
+                        resp.getWriter().println("Hello Servlet");
+                    }
+                }).addMapping("/hello");
+            }
+        });
+        webServer.start();
     }
-
 }
