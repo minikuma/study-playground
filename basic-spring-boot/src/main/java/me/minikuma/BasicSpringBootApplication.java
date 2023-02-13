bfb89258420025d6baf5707c2da90095eadd734c
minikuma <minikuma@naver.com>
2023-02-13 23:03:38 +0900

    FrontController 구현 - without spring

--- basic-spring-boot/src/main/java/me/minikuma/BasicSpringBootApplication.java
+++ basic-spring-boot/src/main/java/me/minikuma/BasicSpringBootApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class BasicSpringBootApplication {
     public static void main(String[] args) {
@@ -18,15 +23,30 @@ public class BasicSpringBootApplication {
         WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
             @Override
             public void onStartup(ServletContext servletContext) throws ServletException {
-                servletContext.addServlet("hello", new HttpServlet() {
+
+                HelloController helloController = new HelloController();
+
+                servletContext.addServlet("frontController", new HttpServlet() {
                     @Override
                     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        resp.setHeader("Content-Type", "text/plain");
-                        resp.setCharacterEncoding("UTF-8");
-                        resp.setStatus(200);
-                        resp.getWriter().println("Hello Servlet");
+                        // 인증, 보안, 다국어, 공통 기능 필요
+                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                            String name = req.getParameter("name"); // (1) Mapping
+
+                            String ret = helloController.hello(name); // (2) Binding
+
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+                            resp.setStatus(HttpStatus.OK.value());
+
+                            resp.getWriter().println(ret);
+                        } else if (req.getRequestURI().equals("/user")) {
+                            // do something
+                        } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+                        }
                     }
-                }).addMapping("/hello");
+                }).addMapping("/*");
             }
         });
         webServer.start();
