6d34c0511a72327c49ddbf93f9adfbd86a712b59
minikuma <minikuma@naver.com>
2023-02-16 22:58:21 +0900

    Base on java code Spring Configuration

--- basic-spring-boot/src/main/java/me/minikuma/BasicSpringBootApplication.java
+++ basic-spring-boot/src/main/java/me/minikuma/BasicSpringBootApplication.java
@@ -2,58 +2,79 @@ package me.minikuma;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
+@Configuration
 public class BasicSpringBootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
         // Spring Container 생성
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            // 기능 확장
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                // 디스패치서블릿 등록 작업
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                                    new DispatcherServlet(this))
+                            .addMapping("/*");
+                });
+
+                webServer.start();
+            }
+        };
+
+        // applicationContext.registerBean(HelloController.class);
+        // applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(BasicSpringBootApplication.class);
         applicationContext.refresh();
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-            @Override
-            public void onStartup(ServletContext servletContext) throws ServletException {
-                // HelloController helloController = new HelloController();
-                servletContext.addServlet("frontController", new HttpServlet() {
-                    @Override
-                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        // 인증, 보안, 다국어, 공통 기능 필요
-                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                            String name = req.getParameter("name"); // (1) Mapping
-
-                            HelloController helloController = applicationContext.getBean(HelloController.class);
-                            String ret = helloController.hello(name); // (2) Binding
-
-                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                            resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-                            resp.setStatus(HttpStatus.OK.value());
-
-                            resp.getWriter().println(ret);
-                        } else if (req.getRequestURI().equals("/user")) {
-                            // do something
-                        } else {
-                            resp.setStatus(HttpStatus.NOT_FOUND.value());
-                        }
-                    }
-                }).addMapping("/*");
-            }
-        });
-        webServer.start();
+//[DispatcherServlet 적용을 위한 코드 정리]
+//        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+//            @Override
+//            public void onStartup(ServletContext servletContext) throws ServletException {
+        // HelloController helloController = new HelloController();
+//                servletContext.addServlet("dispatcherServlet", new HttpServlet() {
+//                    @Override
+//                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                        // 인증, 보안, 다국어, 공통 기능 필요
+//                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+//                            String name = req.getParameter("name"); // (1) Mapping
+//
+//                            HelloController helloController = applicationContext.getBean(HelloController.class);
+//                            String ret = helloController.hello(name); // (2) Binding
+//
+//                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+//                            resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//                            resp.setStatus(HttpStatus.OK.value());
+//
+//                            resp.getWriter().println(ret);
+//                        } else if (req.getRequestURI().equals("/user")) {
+//                            // do something
+//                        } else {
+//                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+//                        }
+//                    }
+//                }).addMapping("/*");
+//            }
+//        });
+//        webServer.start();
+//    }
     }
 }
--- basic-spring-boot/src/main/java/me/minikuma/HelloController.java
+++ basic-spring-boot/src/main/java/me/minikuma/HelloController.java
@@ -1,13 +1,21 @@
 package me.minikuma;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
+
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         // SimpleHelloService simpleHelloService = new SimpleHelloService();
         return helloService.sayHello(Objects.requireNonNull(name));
