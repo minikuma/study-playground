83d4467a2bbeb15c87078f52b53146e5fe067b65
minikuma <minikuma@naver.com>
2023-03-02 22:39:43 +0900

    자동구성 - finished

--- basic-spring-boot/src/main/java/me/config/autoconfig/DispatcherServletConfig.java
+++ basic-spring-boot/src/main/java/me/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package me.config.autoconfig;
 
+import me.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
--- basic-spring-boot/src/main/java/me/config/autoconfig/TomcatWebServerConfig.java
+++ basic-spring-boot/src/main/java/me/config/autoconfig/TomcatWebServerConfig.java
@@ -1,11 +1,11 @@
 package me.config.autoconfig;
 
+import me.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
