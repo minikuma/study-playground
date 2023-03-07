c2edcef9f22920853ffc5160c84d8c5e817e93c6
minikuma <minikuma@naver.com>
2023-03-07 23:05:08 +0900

    conditional 확장

--- basic-spring-boot/src/main/java/me/config/autoconfig/JettyWebServerConfig.java
+++ basic-spring-boot/src/main/java/me/config/autoconfig/JettyWebServerConfig.java
@@ -1,26 +1,18 @@
 package me.config.autoconfig;
 
+import me.config.ConditionalMyOnClass;
 import me.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean(name = "jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
     }
 }
--- basic-spring-boot/src/main/java/me/config/autoconfig/UndertowWebServerConfig.java
+++ basic-spring-boot/src/main/java/me/config/autoconfig/UndertowWebServerConfig.java
@@ -3,16 +3,16 @@ package me.config.autoconfig;
 import me.config.ConditionalMyOnClass;
 import me.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-public class TomcatWebServerConfig {
-    @Bean(name = "tomcatWebServerFactory")
+@ConditionalMyOnClass("io.undertow.Undertow")
+public class UndertowWebServerConfig {
+    @Bean(name = "undertowWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory webServerFactory() {
+        return new UndertowServletWebServerFactory();
     }
 }
