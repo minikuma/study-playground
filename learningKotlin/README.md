d3967e102b309a4c325b8b7dbb39c7ae8a7e53f9
minikuma <minikuma@naver.com>
2023-04-03 23:05:19 +0900

    initial commit (chapter 2까지)

---
+++ learningKotlin/README.md
@@ -0,0 +1,9 @@
+### Basic Kotlin
+    
+
+(1) 변수 선언 (Variable Declaration)
+* ```var``` vs ```val``` 의 차이점
+* ```Strings``` 처리
+
+
+---
---
+++ learningKotlin/src/main/java/com/virtualpairprogrammers/learningkotlin/java/BoringJavaCode.java
@@ -0,0 +1,7 @@
+package com.virtualpairprogrammers.learningkotlin.java;
+
+public class BoringJavaCode {
+    public static void main(String[] args) {
+        String name = "Java";
+    }
+}
