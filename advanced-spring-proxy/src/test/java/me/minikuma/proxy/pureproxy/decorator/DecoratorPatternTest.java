8577aedffae6eca014303cb667add3b3cb0759ba
minikuma <minikuma@naver.com>
2023-06-14 23:27:55 +0900

    Decorator pattern

---
+++ advanced-spring-proxy/src/test/java/me/minikuma/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,32 @@
+package me.minikuma.proxy.pureproxy.decorator;
+
+import lombok.extern.slf4j.Slf4j;
+import me.minikuma.proxy.pureproxy.decorator.code.*;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class DecoratorPatternTest {
+    @Test
+    void noDecorator() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
---
+++ advanced-spring-proxy/src/test/java/me/minikuma/proxy/pureproxy/decorator/code/Component.java
@@ -0,0 +1,5 @@
+package me.minikuma.proxy.pureproxy.decorator.code;
+
+public interface Component {
+    String operation();
+}
