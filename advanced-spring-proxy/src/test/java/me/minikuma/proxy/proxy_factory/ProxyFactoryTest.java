50fb4fe3d2aada19326e1a4d59b4f7a185108b58
minikuma <minikuma@naver.com>
2023-07-04 23:10:05 +0900

    AOP - 포인트컷

--- advanced-spring-proxy/src/test/java/me/minikuma/proxy/proxy_factory/ProxyFactoryTest.java
+++ advanced-spring-proxy/src/test/java/me/minikuma/proxy/proxy_factory/ProxyFactoryTest.java
@@ -2,13 +2,15 @@ package me.minikuma.proxy.proxy_factory;
 
 import lombok.extern.slf4j.Slf4j;
 import me.minikuma.proxy.common.advice.TimeAdvice;
+import me.minikuma.proxy.common.service.ConcreteService;
 import me.minikuma.proxy.common.service.Service;
 import me.minikuma.proxy.common.service.ServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class ProxyFactoryTest {
@@ -25,8 +27,45 @@ class ProxyFactoryTest {
 
         proxy.save();
 
-        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
-        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
-        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구체클래스만 있으면 CGLIB 프록시 사용")
+    void concreteProxy() {
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+
+        proxy.call();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB 를 사용하고 클래스 기반 프록시로 동작")
+    void proxyTargetClass() {
+        Service target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        Service proxy = (Service) proxyFactory.getProxy();
+
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
 }
